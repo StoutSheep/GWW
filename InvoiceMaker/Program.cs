33c902999dcff17ba09e11643cca08086847a66c
MatthewShew <shewmj@gmail.com>
2018-05-02 16:40:12 -0700

    added initiliazing of gww tables

--- InvoiceMaker/Program.cs
+++ InvoiceMaker/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 namespace InvoiceMaker
 {
     static class Program
@@ -14,9 +17,79 @@ namespace InvoiceMaker
         [STAThread]
         static void Main()
         {
+
+            InitializeDatabase();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            
         }
+
+
+
+        static void InitializeDatabase()
+        {
+            string connStr = "server=localhost;user=root;database=GWW;port=3306;password=password";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd;
+                string sql;
+                
+
+                sql = "CREATE TABLE IF NOT EXISTS Customers (" +
+                    "StoreID int NOT NULL AUTO_INCREMENT," +
+                    "StoreName varchar(50) NOT NULL," +
+                    "ShippingAddress varchar(50) NOT NULL," +
+                    "StoreContact varchar(50) NOT NULL," +
+                    "PhoneNumber varchar(50) NOT NULL," +
+                    "PaymentTerms varchar(50) NOT NULL," +
+                    "ShippingInstructions varchar(50) NOT NULL," +
+                    "SpecialNotes varchar(50) NOT NULL," +
+                    "PRIMARY KEY (StoreID)" +
+                    ");";
+                cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+
+
+                sql = "CREATE TABLE IF NOT EXISTS Products (" +
+                    "ItemNo varchar(10) NOT NULL," +
+                    "ItemDesc varchar(50) NOT NULL," +
+                    "PerCarton int NOT NULL," +
+                    "Location varchar(10) NOT NULL," +
+                    "Cost decimal(10,2) NOT NULL," +
+                    "SellPrice decimal(10,2) NOT NULL," +
+                    "UPC varchar(20) NOT NULL," +
+                    "PRIMARY KEY (ItemNo)" +
+                    ");";
+                cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+
+                sql = "CREATE TABLE IF NOT EXISTS ProvinceTax (" +
+                   "Province char(2) NOT NULL," +
+                   "Tax int NOT NULL," +
+                   "CHECK (Tax >= 0 AND Tax <=30)," +
+                   "PRIMARY KEY (Province)" +
+                   ");";
+                cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+
+        }
+
+
+
     }
 }
