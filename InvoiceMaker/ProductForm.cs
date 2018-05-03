01d96e8900688729090f59166abdab6dba83b52c
Jeffrey Chou <jeffreychou1991@gmail.com>
2018-05-03 16:22:12 -0700

    added data insertion on submit click. started on validation of product form

--- InvoiceMaker/ProductForm.cs
+++ InvoiceMaker/ProductForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,6 +14,14 @@ namespace InvoiceMaker
 {
     public partial class ProductForm : Form
     {
+        String itemNo = "";
+        String itemDesc = "";
+        int perCarton;
+        String location = "";
+        double cost;
+        double sellPrice;
+        String upc = "";
+
         public ProductForm()
         {
             InitializeComponent();
@@ -25,7 +34,55 @@ namespace InvoiceMaker
 
         private void submit_button_Click(object sender, EventArgs e)
         {
-            Debug.Print(itemNumber_textBox.Text);
+            itemNo = itemNumber_textBox.Text;
+            itemDesc = itemDescription_textBox.Text;
+            if(cartonPack_textBox.Text.Length > 0)
+                perCarton = Int32.Parse(cartonPack_textBox.Text);
+            location = warehouseLoc_textBox.Text;
+            if (cost_textBox.Text.Length > 0)
+                cost = Double.Parse(cost_textBox.Text);
+            if (sellingPrice_textBox.Text.Length > 0)
+                sellPrice = Double.Parse(sellingPrice_textBox.Text);
+            upc = upc_textBox.Text;
+            if(!validItemNumber(itemNo))
+            {
+                Debug.Print("Failed");
+                return;
+            }
+            Program.AddProduct(itemNo, itemDesc, perCarton, location, cost, sellPrice, upc);
+            this.Close();
+        }
+
+        private Boolean validItemNumber(String itemNo)
+        {
+            if(!validStringLength(itemNo, 10))
+            {
+                return false;
+            }
+            if(!isUniqueItemNo(itemNo))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private Boolean validStringLength(String text, int max)
+        {
+            if (text.Length <= 0 || text.Length > max)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private Boolean isUniqueItemNo(String itemNo)
+        {
+            ArrayList result = Program.SearchProductsByItemNo(itemNo);
+            if(result.Count > 0)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
--- InvoiceMaker/Program.cs
+++ InvoiceMaker/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace InvoiceMaker
 {
@@ -14,9 +17,203 @@ namespace InvoiceMaker
         [STAThread]
         static void Main()
         {
+            InitializeDatabase();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        static void InitializeDatabase()
+        {
+            string connStr = "server=localhost;user=root;database=GWW;port=3306;password=password";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd;
+                string sql;
+
+
+                sql = "CREATE TABLE IF NOT EXISTS Customers (" +
+                    "StoreID int NOT NULL AUTO_INCREMENT," +
+                    "StoreName varchar(50) NOT NULL," +
+                    "ShippingAddress varchar(50) NOT NULL," +
+                    "StoreContact varchar(50) NOT NULL," +
+                    "PhoneNumber varchar(50) NOT NULL," +
+                    "PaymentTerms varchar(50) NOT NULL," +
+                    "ShippingInstructions varchar(50) NOT NULL," +
+                    "SpecialNotes varchar(50) NOT NULL," +
+                    "PRIMARY KEY (StoreID)" +
+                    ");";
+                cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+
+
+                sql = "CREATE TABLE IF NOT EXISTS Products (" +
+                    "ItemNo varchar(10) NOT NULL," +
+                    "ItemDesc varchar(50) NOT NULL," +
+                    "PerCarton int NOT NULL," +
+                    "Location varchar(10) NOT NULL," +
+                    "Cost decimal(10,2) NOT NULL," +
+                    "SellPrice decimal(10,2) NOT NULL," +
+                    "UPC varchar(20) NOT NULL," +
+                    "PRIMARY KEY (ItemNo)" +
+                    ");";
+                cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+
+                sql = "CREATE TABLE IF NOT EXISTS ProvinceTax (" +
+                   "Province char(2) NOT NULL," +
+                   "Tax int NOT NULL," +
+                   "CHECK (Tax >= 0 AND Tax <=30)," +
+                   "PRIMARY KEY (Province)" +
+                   ");";
+                cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+
+        }
+
+
+
+        static void AddCustomer(String storeName, String shippingAddress, String storeContact, int phoneNumber,
+            String PaymentTerms, String ShippingInstructions, String SpecialNotes)
+        {
+            string connStr = "server=localhost;user=root;database=GWW;port=3306;password=password";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd;
+                string sql;
+
+                sql = "INSERT INTO Products (StoreName, ShippingAddress, StoreContact, PhoneNumber, PaymentTerms, ShippingInstructions, SpecialNotes) " +
+                    "VALUES (" +
+                    "'" + storeName + "'," +
+                    "'" + shippingAddress + "'," +
+                    "'" + storeContact + "'," +
+                    "'" + phoneNumber + "'," +
+                    "'" + PaymentTerms + "'," +
+                    "'" + ShippingInstructions + "'," +
+                    "'" + SpecialNotes + "'," +
+                    ");";
+                cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+
+        }
+
+        public static void AddProduct(String itemNo, String itemDesc, int perCarton, String location, double cost, double sellPrice, String upc)
+        {
+            string connStr = "server=localhost;user=root;database=GWW;port=3306;password=password";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd;
+                string sql;
+
+                sql = "INSERT INTO Products VALUES (" +
+                    "'" + itemNo + "'," +
+                    "'" + itemDesc + "'," +
+                    "" + perCarton + "," +
+                    "'" + location + "'," +
+                    "" + cost + "," +
+                    "" + sellPrice + "," +
+                    "'" + upc + "'" +
+                    ");";
+                cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+
+        }
+
+
+        static void AddProvinceTax(String province, int tax)
+        {
+            string connStr = "server=localhost;user=root;database=GWW;port=3306;password=password";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd;
+                string sql;
+
+                sql = "INSERT INTO PovinceTax VALUES (" +
+                    "'" + province + "'," +
+                    "'" + tax + "'," +
+                    ");";
+                cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+
+        }
+
+
+        public static ArrayList SearchProductsByItemNo(String itemNo)
+        {
+            ArrayList result = new ArrayList();
+            string connStr = "server=localhost;user=root;database=GWW;port=3306;password=password";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd;
+                MySqlDataReader rdr;
+                string sql;
+
+                sql = "SELECT ItemNo FROM Products WHERE ItemNo = " + itemNo + ";";  
+                cmd = new MySqlCommand(sql, conn);
+                rdr = cmd.ExecuteReader();
+
+                while(rdr.Read())
+                {
+                    result.Add(rdr[0]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+            return result;
+
         }
     }
 }
